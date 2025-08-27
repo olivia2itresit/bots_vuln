07619240ca02711ccaa4b140b45033985500da47
davidmo-5 <david.montoro@itresit.es>
2025-08-27 13:33:20 +0200

    avance

---
+++ db/create_techs.py
@@ -0,0 +1,28 @@
+import sqlite3
+import csv
+
+DB_FILE = "vulns.db"
+CSV_FILE = "techs_clients.csv"
+
+# Create / connect DB
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
+
+#Create table techs
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS techs (
+    tech TEXT NOT NULL,
+    client TEXT NOT NULL
+)
+""")
+conn.commit()
+
+
+with open(CSV_FILE, newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    filas = [(row['tech'], row['client']) for row in reader]
+
+cursor.executemany("INSERT INTO techs (tech, client) VALUES (?, ?)", filas)
+conn.commit()
+conn.close()
+
--- generic/twitter.py
+++ generic/twitter.py
@@ -5,9 +5,10 @@ import csv
 # Auth
 BEARER_TOKEN1="AAAAAAAAAAAAAAAAAAAAAByM3gEAAAAAxDtbp9ULrgk%2BunPbqrsymv6%2FYwY%3DOphaJ5Pmrb3iVWQo0W7qZiogTrzWcOTDxIZyfy4UoDJEDjDw3H"
 BEARER_TOKEN2="AAAAAAAAAAAAAAAAAAAAAPe93gEAAAAAuoaKLjS7C4sgxdUHHiEn%2BLdOApg%3Dof4AhKUo07BW7ytfHc7RFG7iW2dVleAIUYNgpmekGvRidgYnnr"
+
 # Query
 query = f"""
-("CVE-" OR "0day" OR "exploit" OR "PATCH NOW" OR "patch now") (from:thehackersnews OR from:cert_fr OR from:CISAgov OR from:H4ckmanac) -is:retweet
+("CVE-" OR "0day" OR "exploit" OR "PATCH NOW" OR "patch now" OR "PATCH" OR "patch" OR "Known Exploited Vulnerabilities") (from:thehackersnews OR from:cert_fr OR from:CISAgov OR from:H4ckmanac OR from:CISACyber) -is:retweet
 """
 
 csv_file = "tweets.csv"
@@ -21,16 +22,23 @@ def try_to_fetch_tweets(query):
             return fetch_tweets(query, BEARER_TOKEN2)
         except Exception as e:
             print(f"Error fetching tweets: {e}")
+            raise Exception("Both Twitter API tokens failed.")
             return None
 
 
 def fetch_tweets(query, bearer_token): 
     client = tweepy.Client(bearer_token=bearer_token)
     tweets = client.search_recent_tweets(query=query, max_results=10, tweet_fields=["id","created_at","text"])
+    for tweet in tweets.data:
+        tweet.text = tweet.text.replace("\n", " ").replace("\r", " ")
     return tweets
 
+
+def check_techs(tweets):
+    pass
+
+
 def to_csv(tweets):
-    # On ouvre le CSV en mode append pour ne pas écraser les données déjà existantes
     with open(csv_file, mode="a", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         for tweet in tweets.data:
@@ -45,24 +53,28 @@ def to_csv(tweets):
 
 def to_db(tweets):
     # Connexion DB
-    conn = sqlite3.connect("cves.db")
+    conn = sqlite3.connect("tweets.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS tweets (id TEXT PRIMARY KEY, date TEXT, text TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS tweets (id TEXT PRIMARY KEY, date TEXT, text TEXT, notified INTEGER)")
 
     for tweet in tweets.data:
         # Sauvegarde SQLite
         try:
-            cur.execute("INSERT INTO tweets VALUES (?,?,?)", (tweet.id, str(tweet.created_at), tweet.text))
-        except sqlite3.IntegrityError:
-            pass
+            cur.execute("INSERT INTO tweets VALUES (?,?,?,?) ON CONFLICT(id) DO NOTHING", (tweet.id, str(tweet.created_at), tweet.text, 0))
+            conn.commit()
+    
+        except Exception as e:
+            print(f"Error saving tweet {tweet.id} to DB: {e}")
+            continue
 
-    conn.commit()
     conn.close()
 
 
 def main():
         tweets = try_to_fetch_tweets(query)
         if tweets.data:
+            #TODO Would need a token-against processing to only store tweets relevants to specific tecnologies
+            #tweets = check_techs(tweets)
             to_csv(tweets)
             to_db(tweets)
 
